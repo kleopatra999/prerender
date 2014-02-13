002b5d29757cf8580941ef6ff181383fb95debae
Dave Evans <dave@playcanvas.com>
2014-02-13 16:58:57 +0000

    Added support for AWS credentials from a config file

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 var prerender = require('./lib')
+var AWS = require('aws-sdk');
+var config = {};
+try {
+    config = require('./config');
+} catch (e) {
+    console.warn("No config found")
+}
+
+
+// Setup AWS credentials
+if (config.aws) {
+    var credentials = new AWS.Credentials(config.aws.key, config.aws.secret);
+    AWS.config.update({
+        credentials: credentials,
+        region: config.aws.region
+    });
+    process.env.S3_BUCKET_NAME = config.aws.bucket;    
+}
 
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
@@ -14,6 +32,6 @@ server.use(prerender.blacklist());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
-// server.use(prerender.s3HtmlCache());
+server.use(prerender.s3HtmlCache());
 
 server.start();
